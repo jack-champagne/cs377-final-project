0eb09f1641d1417140fb23d4ca5123bf67e62ed5
Jack Champagne <jackchampagne.r@gmail.com>
2023-05-11 15:00:15 -0400

    mvp starter code

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,64 @@
-use std::{env::args_os, fs::File, io::{Read, BufReader, BufRead}};
+use std::{env::args_os, fs::File, io::{BufReader, BufRead, Read}};
 
 mod myfs;
 
 const BUFFER_SIZE: usize = myfs::BLOCK_SIZE;
+const BUFF: [u8; BUFFER_SIZE] = [b'1'; BUFFER_SIZE];
 
 fn main() {
-    let mut args: Vec<_> = args_os().collect();
+    let args: Vec<_> = args_os().collect();
     if args.len() == 1 {
         eprintln!("usage: {:?} <filename>\n", args_os());
         return;
     }
 
     let my_file_system = myfs::MyFileSystem::new("disk0");
-    let buff: [u8; BUFFER_SIZE] = [b'1'; BUFFER_SIZE];
-    let mut instruction_file = File::open(args[1].as_os_str()).unwrap();
+    let instruction_file = File::open(args[1].as_os_str()).unwrap();
     let reader = BufReader::new(instruction_file);
 
     for result in reader.lines() {
-        let line_string =  result.unwrap();
-        do_file_op(&line_string.as_bytes().to_vec())
+        let mut line_string =  result.unwrap();
+        do_file_op(&my_file_system, &mut line_string)
     }
+    my_file_system.close_disk();
 }
 
-fn do_file_op(line: &Vec<u8>) {
-    match &line[0] {
-        b'C' => todo!(),
-        b'D' => todo!(),
-        op => {
-            let char = *op as char;
-            println!("File operation {char:?} is unimplemented")
+fn get_filename_array(filename: &str) -> [u8; 8] {
+    let mut filename_array: [u8; 8] = [0 ; 8];
+    let bytes = filename.as_bytes();
+    for i in 0..bytes.len().min(8) {
+        filename_array[i] = bytes[i];
+    }
+    filename_array
+}
+
+fn do_file_op(my_fs: &myfs::MyFileSystem, line: &mut String) {
+    let mut split_parts = line.split_ascii_whitespace();
+    let op = split_parts.next().unwrap();
+    let args: Vec<&str> = split_parts.collect();
+    match &op.chars().next().unwrap() {
+        'C' => {
+            let filename = get_filename_array(args[0]);
+            let size = args[1].parse().unwrap();
+            my_fs.create_file(filename, size);
         },
+        'W' => {
+            let filename = get_filename_array(args[0]);
+            let block_num = args[1].parse().unwrap();
+            my_fs.write(filename, block_num, BUFF);
+        },
+        'L' => {
+            my_fs.ls();
+        },
+        'R' => {
+            let filename = get_filename_array(args[0]);
+            let block_num = args[1].parse().unwrap();
+            my_fs.read(filename, block_num); //TODO: Fix returned buffer
+        }
+        'D' => {
+            let filename = get_filename_array(args[0]);
+            my_fs.delete_file(filename);
+        },
+        _ => (),
     }
 }
--- src/myfs.rs
+++ src/myfs.rs
@@ -25,4 +25,30 @@ use std::fs::{File, OpenOptions};
                 }
             }
         }
+
+        pub fn create_file(&self, filename: [u8; 8], size: u8) {
+            println!("creating {:?}: size: {size}", std::str::from_utf8(&filename).unwrap());
+        }
+
+        pub fn delete_file(&self, filename: [u8; 8]) {
+            println!("deleting {:?}", std::str::from_utf8(&filename).unwrap());
+        }
+
+        pub fn ls(&self) {
+            println!("ls'ing")
+        }
+
+        pub fn read(&self, filename: [u8; 8], block_num: u8) -> Option<[u8; BLOCK_SIZE]> {
+            println!("reading {:?}: block #{block_num}", std::str::from_utf8(&filename).unwrap());
+            None
+            // [0; BLOCK_SIZE]
+        }
+
+        pub fn write(&self, filename: [u8; 8], block_num: u8, write_buf: [u8; BLOCK_SIZE]) {
+            println!("writing {:?}: block #{block_num} with block = {:?}", std::str::from_utf8(&filename).unwrap(), String::from_utf8_lossy(&write_buf));
+        }
+
+        pub fn close_disk(self) {
+            drop(self.disk);
+        }
     }
